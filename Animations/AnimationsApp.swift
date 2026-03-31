42ddf17acc736c8570829ad0cd8dbf60d46bc4c0
midkids <83714759+midkids@users.noreply.github.com>
2026-03-30 21:57:17 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Myron Snelson on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
